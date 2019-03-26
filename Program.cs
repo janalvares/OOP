f1defda2177a47374c9b91314a17e06c4613d6e1
jannioalvares <jannio.11@students.amikom.ac.id>
2019-03-26 12:01:52 +0700

    TAXI

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Taxi Taxi = new Taxi();
+
+            Taxi.DriverName = "Jono";
+            Taxi.OnDuty = true;
+            Taxi.Number = 10;
+
+            Taxi.TaxiInfo();
+            Taxi.PickUpPassenger();
+            Taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int Number { get; set; }
+
+        
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine( "Driver Name : {0}", DriverName);
+            Console.WriteLine( "On Duty : {0}", OnDuty ?"Yes" : "No");
+            Console.WriteLine( "Number of Passenger : {0}\n", Number);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sedang mengantar penumpang", DriverName);
+        }
+    }
+}
